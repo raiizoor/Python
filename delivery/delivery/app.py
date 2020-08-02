a47b17514c99519a425c3706526dd8f26c88c3be
raiizoor <mateus.informatica44@gmail.com>
2020-08-02 18:57:57 -0300

    Arrumando as configurações

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,23 +1,9 @@
 from flask import Flask
 
-from delivery.ext import site
 from delivery.ext import config
-from delivery.ext import toolbar
-from delivery.ext import db
-from delivery.ext import migrate
-from delivery.ext import cli
-from delivery.ext import login
-from delivery.ext import admin
 
 
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
-    db.init_app(app)
-    login.init_app(app)
-    admin.init_app(app)
-    migrate.init_app(app)
-    cli.init_app(app)
-    toolbar.init_app(app)
-    site.init_app(app)
     return app
--- delivery/delivery/ext/config/__init__.py
+++ delivery/delivery/ext/config/__init__.py
@@ -1,11 +1,5 @@
+from dynaconf import FlaskDynaconf
+
 def init_app(app):
-    app.config["SECRET_KEY"] = "xesq"
-    app.config["SQLALCHEMY_DATABASE_URI"] ='sqlite:///delivery.db'  
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["FLASK_ADMIN_SWATCH"] ="united" 
-
-
-    if app.debug:
-        app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
-        app.config['DEBUG_TB_PROFILER_ENABLED'] = True
-        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    FlaskDynaconf(app)
+    app.config.load_extensions("EXTENSIONS")
