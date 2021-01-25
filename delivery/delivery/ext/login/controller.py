0b8db307c12b3cc6d36dbeb5980a63a60b589734
Eron Yamamoto Della Justina <35852852+yamamotin@users.noreply.github.com>
2021-01-25 13:32:37 -0300

    adicionado redutor de imagem

--- delivery/delivery/ext/login/controller.py
+++ delivery/delivery/ext/login/controller.py
@@ -1,10 +1,10 @@
 import os, click
+import cv2
+import numpy as np
+
 from werkzeug.security import generate_password_hash
-
 from werkzeug.utils import secure_filename
-
 from flask import current_app as app
-
 from delivery.ext.db.models import User
 from delivery.ext.db import db, models
 
@@ -30,16 +30,17 @@ def create_user(name: str, email: str, password: str, admin:bool = False) -> Use
     return user
 
 def save_user_foto(filename, filestore):
-    """
-    Saves user foto in
-    ./uploads/foo/fasgyda.ext
-    """
-    
     filename = os.path.join(
         app.config["UPLOAD_FOLDER"],
         secure_filename(filename)
-    ) 
-    # TODO:
-    # 1) verificar se o dir existe
-    # 2)criar o diretorio
+    )
     filestore.save(filename)
+    imgresizer(filename)
+
+def imgresizer(path):
+    img = cv2.imread(path)
+    img = cv2.resize(img,(128,128))
+    path = path[0:-4]
+    cv2.imwrite(path+"reduced.png",img)
+# Use nesta funcao o parametro caminho absoluto
+#ex: imgresizer("C:/Users/e-ron/code/Web-delivery/delivery/delivery/uploads/teste.jpg")
