47f3cf173f2308a2b5e9eef2d90dcc21f177d1e3
raiizoor <mateus.informatica44@gmail.com>
2021-03-09 16:13:33 -0300

    Added option for edtion datas in CRUD

--- delivery/delivery/ext/auth/form.py
+++ delivery/delivery/ext/auth/form.py
@@ -31,15 +31,35 @@ class StoresForm(FlaskForm):
     category_id = QuerySelectField(query_factory=lambda: Category.query.all(), allow_blank=False, get_label='name')
     active = wtf.BooleanField("Ativo", default=True)
 
+class StoreEditForm(FlaskForm):
+    name_store = wtf.StringField("Nome da Loja", [wtf.validators.DataRequired()])
+    category_id = QuerySelectField(query_factory=lambda: Category.query.all(), allow_blank=True, get_label='name', blank_text='Selecione uma Categoria')
+    user_id = QuerySelectField(query_factory=lambda: User.query.all(), allow_blank=True, blank_text='Seleciona um usuário')
+    active = wtf.BooleanField("Ativo")
+
 class ItemsForm(FlaskForm):
     name = wtf.StringField("Nome", [wtf.validators.DataRequired()])
     image = FileField("Image")
     price = wtf.FloatField("Preço", [wtf.validators.DataRequired()])
     store_id = QuerySelectField('ID da loja', query_factory=lambda: Store.query.all())
     available = wtf.BooleanField("Disponivel", default=True)
+
+class ItemsEditForm(FlaskForm):
+    name = wtf.StringField("Nome", [wtf.validators.DataRequired()])
+    image = FileField("Image")
+    price = wtf.FloatField("Preço", [wtf.validators.DataRequired()])
+    store_id = QuerySelectField('ID da loja', query_factory=lambda: Store.query.all(), allow_blank=True, blank_text='Selecione uma loja' )
+    available = wtf.BooleanField("Disponivel")
     
 
 class AddressForm(FlaskForm):
+    zip_code = wtf.StringField("CEP", [wtf.validators.DataRequired()])
+    state = wtf.StringField("Estado")               
+    city = wtf.StringField("Cidade")                
+    address= wtf.StringField("Endereço", [wtf.validators.DataRequired()])
+    number_house= wtf.IntegerField("N°", [wtf.validators.DataRequired()])
+
+class AddressEditForm(FlaskForm):
     zip_code = wtf.StringField("CEP", [wtf.validators.DataRequired()])
     state = wtf.StringField("Estado")               
     city = wtf.StringField("Cidade")                
@@ -50,6 +70,11 @@ class OrderForm(FlaskForm):
     created_at = wtf.StringField("Data e Hora")
     completed = wtf.BooleanField("Comprado")
     store_id = QuerySelectField("ID da Loja", query_factory=lambda: Store.query.all())
+
+class OrderEditForm(FlaskForm):
+    created_at = wtf.StringField("Data e Hora")
+    completed = wtf.BooleanField("Comprado")
+    store_id = QuerySelectField("ID da Loja", query_factory=lambda: Store.query.all(), allow_blank=True, blank_text='Selecione uma loja')
 
 class OrderItemsForm(FlaskForm):
     order_id = QuerySelectField("ID ordem de compra", query_factory=lambda: Order.query.all())
--- delivery/delivery/ext/views/routes_registers.py
+++ delivery/delivery/ext/views/routes_registers.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask import current_app as app
 from flask_login import login_required, current_user
-from delivery.ext.auth.form import CategoryForm, ItemsForm, StoresForm, AddressForm, OrderForm, OrderItemsForm
+from delivery.ext.auth.form import CategoryForm, CategoryEditForm, ItemsForm, ItemsEditForm, StoresForm, StoreEditForm, AddressForm,\
+                                    AddressEditForm, OrderForm, OrderEditForm, OrderItemsForm
 from delivery.ext.auth.controller import create_category, create_item, create_store, create_address, create_order, save_user_picture
 from datetime import datetime
 from delivery.ext.db import db
@@ -40,12 +41,29 @@ def register_category():
 
     return render_template("category/register_category.html", form=form)
 
-@category.route('/editar_categoria', methods=['GET', 'POST'])
+@category.route('/editar_categoria/<id>', methods=['GET', 'POST'])
 @login_required
-def edit_category():
-    
+def edit_category(id):
+    form = CategoryEditForm()
+    category = Category.query.filter_by(id=id).first()
 
-    return render_template('category/edit_category.html')
+    if form.validate_on_submit():
+        category.name = form.name.data
+        category.on_menu = form.on_menu.data
+
+        try:
+            db.session.add(category)
+            db.session.commit()
+            flash('Alterado com sucesso', 'success')
+            return redirect(url_for('.list_category'))
+        except Exception:
+            db.session.rollback()
+            flash('Houve algum erro ao editar', 'danger')
+            return redirect(url_for('.list_category'))
+
+    form.name.data = category.name
+    form.on_menu.data = category.on_menu
+    return render_template('category/edit_category.html', category=category, form=form)
 
 @category.route('/deletar_categoria/<id>')
 @login_required
@@ -71,11 +89,39 @@ def delete_store(id):
     flash('Estabelecimento deletado!', 'success')
     return redirect(url_for('.list_store'))
 
-@category.route('/editar_loja', methods=['GET', 'POST'])
+@category.route('/editar_loja/<id>', methods=['GET', 'POST'])
 @login_required
-def edit_store():
+def edit_store(id):
+    form = StoreEditForm()
+    store = Store.query.filter_by(id=id).first()
 
-    return render_template('store/edit_store.html')
+    if form.validate_on_submit():
+        store.name_store = form.name_store.data
+        if form.category_id.data == None:
+            pass
+        else:
+            store.category_id = form.category_id.data.name 
+        if form.user_id.data == None:
+            pass
+        else:
+            store.user_id = form.user_id.data.email
+        store.active = form.active.data
+
+        try:
+            db.session.add(store)
+            db.session.commit()
+            flash('Usuario Editado!', 'success')
+            return redirect(url_for('.list_store'))
+        except Exception:
+            db.session.rollback()
+            flash('Houve um algum erro ao editar!', 'danger')
+            return redirect(url_for('.list_store'))
+
+    form.name_store.data = store.name_store
+    form.category_id.data = store.category_id
+    form.user_id.data = store.user_id
+    form.active.data = store.active
+    return render_template('store/edit_store.html', store=store, form=form)
 
 @category.route('/loja', methods=['GET', 'POST'])
 @login_required
@@ -104,11 +150,36 @@ def list_items():
     item = Items.query.all()
     return render_template('items/list_items.html', item=item)
 
-@category.route('/editar_itens', methods=['GET', 'POST'])
+@category.route('/editar_itens/<id>', methods=['GET', 'POST'])
 @login_required
-def edit_items():
+def edit_items(id):
+    form = ItemsEditForm()
+    items = Items.query.filter_by(id=id).first()
 
-    return render_template('items/edit_items.html')
+    if form.validate_on_submit():
+        items.name = form.name.data
+        items.price = form.price.data
+        if form.store_id.data == None:
+            pass
+        else:
+            items.store_id = form.store_id.data.name_store
+        items.available = form.available.data
+
+        try:
+            db.session.add(items)
+            db.session.commit()
+            flash('Item editado com sucesso', 'success')
+            return redirect(url_for('.list_items'))
+        except Exception:
+            db.session.rollback()
+            flash('Houve algum erro ao editar o item', 'danger')
+            return redirect(url_for('.list_items'))
+
+    form.name.data = items.name
+    form.price.data = items.price
+    form.store_id.data = items.store_id
+    form.available.data = items.available
+    return render_template('items/edit_items.html', items=items, form=form)
 
 @category.route('/editar_itens/<id>')
 @login_required
@@ -153,11 +224,35 @@ def list_address():
     addresses = Address.query.all()
     return render_template('address/list_address.html', addresses=addresses)
 
-@category.route('/editar_endereço', methods=['GET', 'POST'])
+@category.route('/editar_endereço/<id>', methods=['GET', 'POST'])
 @login_required
-def edit_address():
+def edit_address(id):
+    form = AddressEditForm()
+    address = Address.query.filter_by(id=id).first()
+    
+    if form.validate_on_submit():
+        address.zip_code = form.zip_code.data
+        address.state = form.state.data
+        address.city = form.city.data
+        address.address = form.address.data
+        address.number_house = form.number_house.data
 
-    return render_template('address/edit_address.html')
+        try:
+            db.session.add(address)
+            db.session.commit()
+            flash('Endereço editado com sucesso!', 'success')
+            return redirect(url_for('.list_address'))
+        except Exception:
+            db.session.rollback()
+            flash('Houve algum erro ao editar o endereço', 'danger')
+            return redirect(url_for('.list_address'))
+
+    form.zip_code.data = address.zip_code
+    form.state.data = address.state
+    form.city.data = address.city
+    form.address.data = address.address
+    form.number_house.data = address.number_house
+    return render_template('address/edit_address.html', address=address, form=form)
 
 @category.route('/deletar_endereco/<id>')
 @login_required
@@ -197,11 +292,32 @@ def list_order():
     orders = Order.query.all()
     return render_template('order/list_order.html', orders=orders)
 
-@category.route('/editar_ordem', methods=['GET', 'POST'])
+@category.route('/editar_ordem/<id>', methods=['GET', 'POST'])
 @login_required
-def edit_order():
+def edit_order(id):
+    form = OrderEditForm()
+    order = Order.query.filter_by(id=id).first()
 
-    return render_template('order/edit_order.html')
+    if form.validate_on_submit():
+        order.completed = form.completed.data
+        if form.store_id.data == None:
+            pass
+        else:
+            order.store_id  = form.store_id.data.name_store
+
+        try:
+            db.session.add(order)
+            db.session.commit()
+            flash('Compra editada com sucesso!', 'success')
+            return redirect(url_for('.list_order'))
+        except Exception:
+            db.session.rollback()
+            flash('Houve algum erro ao editar a compra', 'danger')
+            return redirect(url_for('.list_order'))
+
+    form.completed.data = order.completed
+    form.store_id.data = order.store_id
+    return render_template('order/edit_order.html', order=order, form=form)
 
 @category.route('/deletar_ordem/<id>')
 @login_required
